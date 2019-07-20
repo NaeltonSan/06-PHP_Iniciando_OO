34ef8323eeec48983c10e24a48bbc8bcc8c3dc27
Naelton Santos <naeon02@gmail.com>
2019-07-19 23:08:51 -0300

    modificações

--- 1-index.php
+++ 1-index.php
@@ -2,30 +2,18 @@
     require_once ("2-Car.php");
     require_once ("4-Motorcycle.php");
 
-    $ferrari = new Car;
-    $ferrari->brand = "Ferrari";
-    $ferrari->color = "Red";
-    $ferrari->engine = 300;
-    $ferrari->doors = 2;
+    //   $ferrari = new Car('Ferrari'); // color vem como null
+    // $ferrari->brand = "Ferrari";
+    // $ferrari->color = "Red";
+    
+    $ferrari = new Car("Ferrari", "Red"); // color vem como Red
 
-    $mustang = new Car;
-    $mustang->brand = "Mustang";
-    $mustang->color = "Blue";
-    $mustang->engine = 200;
-    $mustang->doors = 4;
-
-    $moto = new Motorcycle;
-    $moto->brand = "Honda";
-    $moto->color = "Yellow";
-    $moto->engine = 200;
-     
-    //echo $ferrari->brand;
-    // var_dump($ferrari);
-    // var_dump($mustang);
-
-    //echo $mustang->getEngine();
+    
 
     //echo $ferrari->getEngine();
+
+    echo $ferrari->color;
+    
 
 
     
--- 3-Vehicle.php
+++ 3-Vehicle.php
@@ -6,9 +6,17 @@
         public $color;
         public $engine;
 
+        public function __construct($brand = null, $color = null)// setando valor padrão para estes parametros
+        {
+            $this->brand = $brand;
+            $this->color = $color;
+        }
+        
+
         public function getEngine($type = "horsepower")  //métodos
         {
             return "{$this->engine} {$type}"; 
         }
     }
+    
     
